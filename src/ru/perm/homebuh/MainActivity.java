a5bcecff61d1829acaf9b7e2c639f17a7e00d24a
tral <tralmail@gmail.com>
2013-09-08 05:37:22 +0600

    1

--- src/ru/perm/homebuh/MainActivity.java
+++ src/ru/perm/homebuh/MainActivity.java
@@ -49,7 +49,7 @@ implements CompoundButton.OnCheckedChangeListener//, CompoundButton.
 	            "Корректировки", "Кредиты", "Одежда", "Подарки и праздники", 
 	            "Прочее", "Путешествие", "Счета", "Транспорт", "Электроника"};
 	
-	//test commit 4
+	//test commit 5
 	//Date
     private Button mPickDate;
     private int mYear;
